7f7ba4723183d2a89edf1d3cd313535322814d00
Patryk Czeszejko <s21269@pjwstk.edu.pl>
2024-04-06 22:50:03 +0200

    Initial project

---
+++ zadanie/LegacyApp/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LegacyApp
+{
+    public class User : Client
+    {
+        public object Client { get; internal set; } // dodać dziedziczenie po klasie Client?
+        //public User() : base() { }
+        public DateTime DateOfBirth { get; internal set; }
+        public string EmailAddress { get; internal set; }
+        public string FirstName { get; internal set; }
+        public string LastName { get; internal set; }
+        public bool HasCreditLimit { get; internal set; }
+        public int CreditLimit { get; internal set; }
+
+        public User(string firstName, string lastName, string email, DateTime dateOfBirth)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.EmailAddress = email;
+            this.DateOfBirth = dateOfBirth;
+        }
+    }
+}
---
+++ zadanie/LegacyApp/UserService.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace LegacyApp
+{
+    public class UserService 
+    {
+               
+        public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
+        {
+            if(!CheckInfo(firstName, lastName, email) && !CheckAge(dateOfBirth))
+                return false;
+           
+
+            var clientRepository = new ClientRepository();
+            var client = clientRepository.GetById(clientId);
+
+            var user = CreateUser (firstName, lastName, email, dateOfBirth);
+
+            if (client.Type == "VeryImportantClient")
+            {
+                user.HasCreditLimit = false;
+            }
+            else if (client.Type == "ImportantClient")
+            {
+                using (var userCreditService = new UserCreditService())
+                {
+                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+                    creditLimit = creditLimit * 2;
+                    user.CreditLimit = creditLimit;
+                }
+            }
+            else
+            {
+                user.HasCreditLimit = true;
+                using (var userCreditService = new UserCreditService())
+                {
+                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+                    user.CreditLimit = creditLimit;
+                }
+            }
+
+            if (user.HasCreditLimit && user.CreditLimit < 500)
+            {
+                return false;
+            }
+
+            UserDataAccess.AddUser(user);
+            return true;
+        }
+
+        public bool CheckInfo(string firstName, string lastName, string email)
+        {
+            return (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) && !email.Contains("@") && !email.Contains("."));
+        }
+
+        public bool CheckAge(DateTime dateOfBirth) {
+            var now = DateTime.Now;
+            int age = now.Year - dateOfBirth.Year;
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
+            return age >= 21;
+            }
+
+        public User CreateUser(string firstName, string lastName, string email, DateTime dateOfBirth) {
+
+            return new User (firstName, lastName, email, dateOfBirth);
+            
+        }
+
+    }
+    
+
+}
+
